12cdad060fa89c0e1097e8fcc57ea068717317da
luschettini <luizaschettinii@gmail.com>
2025-04-15 08:47:23 -0300

    feat: add content in pages luiza

---
+++ pages/LuizaDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LuizaDetail() {
+    return (
+    <View style={{ padding: 20 }}>
+        <Text style={{ fontSize: 24 }}>Detalhes da Luiza</Text>
+        <Text>Aqui estão os detalhes do Luiza...</Text>
+    </View>
+    );
+}
